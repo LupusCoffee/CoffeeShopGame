d89955c9c52fecced9d86b0486461d154c35385f
Daniel Mardunovich <92152622+MisterCatt@users.noreply.github.com>
2025-08-27 15:09:45 +0200

    Adding the state tree plugin to the project
    Making sure I can make C++ tasks

--- CoffeeShopGame/Source/CoffeeShopGame/CoffeeShopGame.Build.cs
+++ CoffeeShopGame/Source/CoffeeShopGame/CoffeeShopGame.Build.cs
@@ -8,7 +8,7 @@ public class CoffeeShopGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
@@ -18,6 +18,6 @@ public class CoffeeShopGame : ModuleRules
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true lol kek Yongqin
 	}
 }
